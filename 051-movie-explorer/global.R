99376ab2353cc99b48debf3103126823f1e183fb
philipDilts <philip.dilts@ontario.ca>
2017-03-28 11:48:08 -0400

    github proxy test
    committing just to test github proxy. Not sure what the changes are
    since last commit.

--- 051-movie-explorer/global.R
+++ 051-movie-explorer/global.R
@@ -1,9 +1,7 @@
 # Variables that can be put on the x and y axes
 axis_vars <- c(
-  "Tomato Meter" = "Meter",
-  "Numeric Rating" = "Rating",
-  "Number of reviews" = "Reviews",
-  "Dollars at box office" = "BoxOffice",
-  "Year" = "Year",
-  "Length (minutes)" = "Runtime"
+  "Total Registrations" = "total_reg",
+  "Completion Rate" = "pct_complete",
+  "Hourly Wage" = "wage_hourly_avg",
+  "Percent of Female Apprentices" = "pct_fem"
 )
